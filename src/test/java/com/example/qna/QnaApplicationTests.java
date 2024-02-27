983de5a03359bdb350511c7197d11c6edbbcf2d9
jjeongdong <donghun3296@naver.com>
2024-02-28 01:25:30 +0900

    chore : 테스트 코드 삭제

--- src/test/java/com/example/qna/QnaApplicationTests.java
+++ src/test/java/com/example/qna/QnaApplicationTests.java
@@ -1,54 +1,12 @@
 package com.example.qna;
 
-import com.example.qna.entity.Answer;
-import com.example.qna.entity.Question;
-import com.example.qna.repository.AnswerRepository;
-import com.example.qna.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
-    private AnswerRepository answerRepository;
-
     @Test
-    void testJpa1() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);  // 두번째 질문 저장
-    }
-
-    @Test
-    void testJpa2() {
-        Optional<Question> oq = this.questionRepository.findById(2L);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a);
+    void contextLoads() {
     }
 
 }
